ea98bcda10150f8307ca3735b9cab85f7f116fc9
Alecell <alecell.dev@gmail.com>
2021-09-26 01:08:35 -0300

    fix: minor adjusts such as loading, doctype and so on

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
-async function init() {
+window.onload = async function() {
   const source = document.getElementById('item-template').innerHTML;
   const list = document.getElementById('list');
   const template = Handlebars.compile(source);
   const users = await requestData;
+  let $items = [];
 
-  $items = template(users);
-
-  list.innerHTML = $items;
-}
-
-window.onload = init;
+  if (users.length) {
+    $items = template(users);
+    list.innerHTML = $items;
+  } else {
+    list.innerHTML = 'Something went wrong with the API, try again later';
+  }
+};
